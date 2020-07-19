b8de0a9a1bc10cdd867c9b552b89e4cd3904d447
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-19 12:18:48 -0600

    flatlist

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,13 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const BlogPost = [
+    { title: 'Blog # 1'},
+    { title: 'Blog # 2'}
+  ];
+
   return (
-    <BlogContext.Provider value={15}>
+    <BlogContext.Provider value={BlogPost}>
       {children}
     </BlogContext.Provider>
   )
