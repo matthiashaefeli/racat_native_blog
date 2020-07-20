80367ff79c8b08c63988a4224a77d285dc200365
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-20 15:02:16 -0600

    add show screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { EvilIcons } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
@@ -17,12 +17,14 @@ const IndexScreen = () => {
         keyExtractor={(BlogPost) => BlogPost.title}
         renderItem={({ item }) => {
           return (
+          <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
             <View style={styles.row}>
               <Text style={styles.title}>{item.title}</Text>
               <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
                 <EvilIcons name="trash" style={styles.icon} />
               </TouchableOpacity>
             </View>
+          </TouchableOpacity>
           )
         }}
       />
