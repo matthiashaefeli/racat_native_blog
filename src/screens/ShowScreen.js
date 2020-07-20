b012a2316a22243d83f85089500edd4b5f9afbbd
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-20 15:11:15 -0600

    show blog in showScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = (props) => {
+  const { state } = useContext(Context)
+
+  const blogPost = state.find((blogPost) => blogPost.id === props.route.params.id)
+
   return (
     <View>
-      <Text>show screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   )
 };
