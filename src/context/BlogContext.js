11aeafdaa485a54a3e7f80a9b52bb654175e17ea
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-22 14:29:21 -0600

    finished

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -9,13 +9,6 @@ const blogReducer = (state, action) => {
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
-    case 'add_blogpost':
-      return [
-          ...state,
-          { id: Math.floor(Math.random() * 99999),
-            title: action.payload.title,
-            content: action.payload.content }
-      ];
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload );
     default:
@@ -34,7 +27,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content })
-    // dispatch({ type: 'add_blogpost', payload: { title, content } });
     if (callback) {
       callback();
     }
@@ -42,13 +34,17 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
+    
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({type: 'edit_blogpost', payload: { id, title, content } })
     if (callback) {
       callback();
