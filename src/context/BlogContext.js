0dc0ae4c52f8266bab5662ddb49b8b4b80aa8cd9
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-22 11:02:59 -0600

    add json server

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload;
     case 'edit_blogpost':
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -19,6 +22,14 @@ const blogReducer = (state, action) => {
       return state;
   }
 };
+
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    
+    dispatch({ type: 'get_blogposts', payload: response.data });
+  }
+}
 
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
@@ -46,6 +57,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: 'test title', content: 'test content', id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
